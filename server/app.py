6a8585e26a83a8aa66240d57a0093e081e2f80b7
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-28 16:57:28 -0600

    solution, set up login, checksession, logout

--- server/app.py
+++ server/app.py
@@ -47,10 +47,37 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+
+        if user:
+            session['user_id'] = user.id
+            return UserSchema().dump(user)
+        else:
+            return {'message': 'Invalid login'}, 401
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return UserSchema().dump(user)
+        else:
+            return {}, 401
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Logout, '/logout')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
